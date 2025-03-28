3d91fcedc1a107458920b73c054b4d8858fa5a11
Filippo <filippo@web3.foundation>
2025-03-28 19:10:11 +0100

    initial edits

---
+++ rust-state-machine/target/debug/deps/rust_state_machine-aa3aa271bc1b9dd3.d
@@ -0,0 +1,5 @@
+/Users/filippo/Documents/GitHub/rust-state-machine/rust-state-machine/target/debug/deps/rust_state_machine-aa3aa271bc1b9dd3: src/main.rs
+
+/Users/filippo/Documents/GitHub/rust-state-machine/rust-state-machine/target/debug/deps/rust_state_machine-aa3aa271bc1b9dd3.d: src/main.rs
+
+src/main.rs:
