3266bf7a729d7c95297156220a230bfd9d7bc452
Filippo <filippo@web3.foundation>
2025-03-29 13:04:38 +0100

    make balances config

--- rust-state-machine/src/main.rs
+++ rust-state-machine/src/main.rs
@@ -12,13 +12,18 @@ mod types {
 #[derive(Debug)]
 pub struct Runtime {
 	system: system::Pallet<Self>,
-	balances: balances::Pallet<types::AccountId, types::Balance>,
+	balances: balances::Pallet<Self>,
 }
 
 impl system::Config for Runtime {
     type AccountId = types::AccountId;
     type BlockNumber = types::BlockNumber;
     type Nonce = types::Nonce;
+}
+
+impl balances::Config for Runtime {
+    type AccountId = types::AccountId;
+    type Balance = types::Balance;
 }
 
 impl Runtime {
@@ -53,39 +58,3 @@ fn main() {
 	
 	println!("{:#?}", runtime);
 }
-
-#[cfg(test)]
-mod tests {
-    use crate::balances;
-
-    #[test]
-    fn init_balances() {
-        let mut balances = balances::Pallet::new();
-
-        assert_eq!(balances.balance(&"alice".to_string()), 0);
-        balances.set_balance(&"alice".to_string(), 100);
-        assert_eq!(balances.balance(&"alice".to_string()), 100);
-        assert_eq!(balances.balance(&"bob".to_string()), 0);
-    }
-
-    #[test]
-    fn transfer_balance() {
-        let mut balances = balances::Pallet::new();
-
-        assert_eq!(
-            balances.transfer("alice".to_string(), "bob".to_string(), 51),
-            Err("Not enough funds.")
-        );
-
-        balances.set_balance(&"alice".to_string(), 100);
-        assert_eq!(balances.transfer("alice".to_string(), "bob".to_string(), 51), Ok(()));
-        assert_eq!(balances.balance(&"alice".to_string()), 49);
-        assert_eq!(balances.balance(&"bob".to_string()), 51);
-
-        assert_eq!(
-            balances.transfer("alice".to_string(), "bob".to_string(), 51),
-            Err("Not enough funds.")
-        );
-    }
-}
-
