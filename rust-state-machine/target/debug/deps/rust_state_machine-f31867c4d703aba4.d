158eec1ccf78f44dc33f65f2419138de16e51e18
Filippo <filippo@web3.foundation>
2025-03-29 20:37:21 +0100

    edits

--- rust-state-machine/target/debug/deps/rust_state_machine-f31867c4d703aba4.d
+++ rust-state-machine/target/debug/deps/rust_state_machine-f31867c4d703aba4.d
@@ -1,7 +1,8 @@
-/Users/filippo/Documents/GitHub/rust-state-machine/rust-state-machine/target/debug/deps/librust_state_machine-f31867c4d703aba4.rmeta: src/main.rs src/balances.rs src/system.rs
+/Users/filippo/Documents/GitHub/rust-state-machine/rust-state-machine/target/debug/deps/librust_state_machine-f31867c4d703aba4.rmeta: src/main.rs src/balances.rs src/system.rs src/support.rs
 
-/Users/filippo/Documents/GitHub/rust-state-machine/rust-state-machine/target/debug/deps/rust_state_machine-f31867c4d703aba4.d: src/main.rs src/balances.rs src/system.rs
+/Users/filippo/Documents/GitHub/rust-state-machine/rust-state-machine/target/debug/deps/rust_state_machine-f31867c4d703aba4.d: src/main.rs src/balances.rs src/system.rs src/support.rs
 
 src/main.rs:
 src/balances.rs:
 src/system.rs:
+src/support.rs:
